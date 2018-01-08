d2a2cae5e8cc5cb4fd4dc812d233bac7d9242edc
Chung, Kuan-Wu <521chung@gmail.com>
2018-01-08 16:57:33 +0800

    Subscribing to Notifications

--- JJ/firebase/js/main.js
+++ JJ/firebase/js/main.js
@@ -10,6 +10,8 @@
 
   const signInButton = document.getElementById('sign-in');
   const signOutButton = document.getElementById('sign-out');
+  const subscribeButton = document.getElementById('subscribe');
+  const unsubscribeButton = document.getElementById('unsubscribe');
 
   /* ========================
     Event Listeners
@@ -17,8 +19,11 @@
 
   signInButton.addEventListener('click', signIn);
   signOutButton.addEventListener('click', signOut);
+  subscribeButton.addEventListener('click', subscribeToNotification);
+  unsubscribeButton.addEventListener('click', unsubscribeFromNotification);
 
   FIREBASE_AUTH.onAuthStateChanged(handleAuthStateChanged);
+  FIREBASE_MESSAGING.onTokenRefresh(handleTokenRefresh);
 
   /* ========================
     Functions
@@ -37,11 +42,55 @@
       console.log(user);
       signInButton.setAttribute('hidden', 'true');
       signOutButton.removeAttribute('hidden');
+      checkSubscrption();
     } else {
       console.log('No User!');
       signOutButton.setAttribute('hidden', 'true');
       signInButton.removeAttribute('hidden');
     }
+  }
+
+  function subscribeToNotification() {
+    FIREBASE_MESSAGING.requestPermission()
+      .then(() => handleTokenRefresh())
+      .then(() => checkSubscrption())
+      .catch(() => console.log("User didn't give permission :("));
+  }
+
+  function handleTokenRefresh() {
+    return FIREBASE_MESSAGING.getToken()
+      .then((token) => {
+        FIREBASE_DATABASE.ref('/tokens').push({
+          token: token,
+          uid: FIREBASE_AUTH.currentUser.uid
+        });
+      });
+  }
+
+  function unsubscribeFromNotification() {
+    FIREBASE_MESSAGING.getToken()
+      .then((token) => FIREBASE_MESSAGING.deleteToken(token))
+      .then(() => FIREBASE_DATABASE.ref('/tokens').orderByChild('uid').equalTo(FIREBASE_AUTH.currentUser.uid).once('value'))
+      .then((snapshot) => {
+        console.log(snapshot.val());
+        const key = Object.keys(snapshot.val())[0];
+        return FIREBASE_DATABASE.ref('/tokens').child(key).remove();
+      })
+      .then(() => checkSubscrption())
+      .catch(() => console.log("Unsubscribe Failed :("));
+  }
+
+  function checkSubscrption() {
+    FIREBASE_DATABASE.ref('/tokens').orderByChild('uid').equalTo(FIREBASE_AUTH.currentUser.uid).once('value')
+      .then((snapshot) => {
+        if (snapshot.val()) {
+          subscribeButton.setAttribute('hidden', 'true');
+          unsubscribeButton.removeAttribute('hidden');
+        } else {
+          unsubscribeButton.setAttribute('hidden', 'true');
+          subscribeButton.removeAttribute('hidden');
+        }
+      })
   }
 
 }
