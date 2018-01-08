9a70dd0a6c44543cda30d8567bcdfc75b11e3d6d
Chung, Kuan-Wu <521chung@gmail.com>
2018-01-08 16:19:46 +0800

    Authentication

--- JJ/firebase/js/main.js
+++ JJ/firebase/js/main.js
@@ -1,19 +1,47 @@
 {
 
-/* ========================
-  Variables
-======================== */
+  /* ========================
+    Variables
+  ======================== */
 
-const FIREBASE_AUTH = firebase.auth();
-const FIREBASE_MESSAGING = firebase.messaging();
-const FIREBASE_DATABASE = firebase.database();
+  const FIREBASE_AUTH = firebase.auth();
+  const FIREBASE_MESSAGING = firebase.messaging();
+  const FIREBASE_DATABASE = firebase.database();
 
-/* ========================
-  Event Listeners
-======================== */
+  const signInButton = document.getElementById('sign-in');
+  const signOutButton = document.getElementById('sign-out');
 
-/* ========================
-  Functions
-======================== */
+  /* ========================
+    Event Listeners
+  ======================== */
+
+  signInButton.addEventListener('click', signIn);
+  signOutButton.addEventListener('click', signOut);
+
+  FIREBASE_AUTH.onAuthStateChanged(handleAuthStateChanged);
+
+  /* ========================
+    Functions
+  ======================== */
+
+  function signIn() {
+    FIREBASE_AUTH.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  function signOut() {
+    FIREBASE_AUTH.signOut();
+  }
+
+  function handleAuthStateChanged(user) {
+    if (user) {
+      console.log(user);
+      signInButton.setAttribute('hidden', 'true');
+      signOutButton.removeAttribute('hidden');
+    } else {
+      console.log('No User!');
+      signOutButton.setAttribute('hidden', 'true');
+      signInButton.removeAttribute('hidden');
+    }
+  }
 
 }
